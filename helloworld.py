fe0167bd4ee0b75277a37aa3dd581ad47b1d6e3e
PadduBhagi <122661871+PadduBhagi@users.noreply.github.com>
2025-11-26 13:53:12 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,20 @@
+class MathsOperations:
+
+  def add(self,a,b):
+    c=a+b
+    print(c)
+  def sub(self,a,b):
+    c=a-b
+    print(c)
+  def mul(self,a,b):
+    c=a*b
+    print(c)
+  def div(self,a,b):
+    c=a/b
+    print(c)
+
+obj=MathsOperations()
+obj.add(10,5)
+obj.sub(10,5)
+obj.mul(10,5)
+obj.div(10,5)
